24ff791d56622c4d7f169c1fc75311708dfa3b77
Guillermo Riaño <guilledark17@gmail.com>
2025-06-16 21:40:51 -0600

    🎉Initial: Add Project Folder

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+    public Text UIText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int count = 1;
+        //do
+        //{
+        //    Debug.Log($"{count}: Hi World");
+        //    count++;
+        //} while (count <= 10);
+        Debug.Log($"Hi World");
+        UIText.text = $"Hi World!";
+    }
+}
